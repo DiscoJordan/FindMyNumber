8d3c0279010bf96d608237943ea08686d8f2c43a
Gleb Muravitsky <56343088+DiscoJordan@users.noreply.github.com>
2024-05-31 09:44:46 +0200

    added some styles

--- script.js
+++ script.js
@@ -3,17 +3,22 @@ let guesses = 5;
 const again = document.getElementById("again")
 const form = document.getElementById("form")
 const title = document.getElementById("title")
+const button = document.getElementById("guessbutton")
+const guessesTitle = document.getElementById("window__guesses")
+const html = document.getElementById("html")
 const actArr = [again, form, title]
 console.log(rightnumber);
 
-form.addEventListener("submit", function (event) {
-    event.preventDefault()
+function handleGuess() {
     let number = document.getElementById('input').value;
     if (number > 10 || number < 1) {
         document.getElementById('str').innerHTML = `Write a number between 1-10!`;
     } else if (number == rightnumber && guesses > 0) {
         document.getElementById('str').innerHTML = `${number} is a right number!`;
-        document.getElementById('window__guesses').innerHTML = `Congratulations, right number! Want to try again?`;
+        document.getElementById('header1').innerHTML = `Congratulations`;
+        document.getElementById('window__guesses').innerHTML = `Guessed right on the ${6-guesses} try`;
+        html.classList.add("active")
+        
 
         actArr.forEach(ele => {
             ele.classList.add("active")
@@ -24,23 +29,25 @@ form.addEventListener("submit", function (event) {
         document.getElementById('window__guesses').innerHTML = `You have ${guesses} guesses`;
     }
     else {
-        document.getElementById('window__guesses').innerHTML = `No more guesses :(( `;
+       
+        document.getElementById('header1').innerHTML = `No more guesses :((`;
+        html.classList.add("lose")
+        guessesTitle.classList.add("active")
 
         actArr.forEach(ele => {
             ele.classList.add("active")
         })
-
-
-
-      
-
     }
     document.getElementById('input').value = ''
+}
 
-
-
+form.addEventListener("submit", function (event) {
+    event.preventDefault()
+   handleGuess()
 })
 
-
+button.addEventListener("click", function () {
+   handleGuess()
+})
 
 
